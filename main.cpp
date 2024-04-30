f899bb5c228e7ae77d71a01381fa549e3ac54768
Julian Vara <jvara6282@gmail.com>
2024-04-29 23:30:12 -0700

    Working on the login function and converting the input to binary and store the data

--- main.cpp
+++ main.cpp
@@ -6,21 +6,35 @@
 
 #include <iostream> 
 #include <string> 
-
+#include <fstream> //This header allows us to read and write to bin files 
+#include <cstring>
 
 using namespace std; 
 
 //The user needs some kind of search bar for the website part 
-struct Items
-{
+class Items
+{   
+    public:
     string Backpacks; 
     string Jackets; 
     string Cars;
 };
 
-void greet();
+class Users
+{
+    public:
+    string userName; 
+    string userPassword; 
+    char n[12]; //This is for User NAME
+    char p[12]; //This is for PASSWORD
+};
+
+void userMenu(Users);
 int pickItem();
 void itemChose(int, Items);
+//void login(); 
+void signUp(Users);
+
 
 int main()
 {
@@ -28,9 +42,11 @@ int main()
     items.Backpacks = "Jansport";
     items.Jackets = "Patagonia"; 
     items.Cars = "Subaru";
+
+    Users tempUser;
     
 
-    greet();
+    userMenu(tempUser);
     int choice = pickItem();
     //Trying to return the value from the pickItem function to the itemChose function 
     //So I know what the user choose from the pickItem function :)
@@ -39,11 +55,90 @@ int main()
 
 // This function introductions the user to the program and explains the 
 // functionalilty of the program
-void greet()
+void userMenu(Users tempUsers)
 {
-    cout << "Welcome to Julian's Store!" << endl; 
-    cout << "This program is like Amazon but you cannot actually buy anything!" << endl; 
+    int input = 0; 
+    cout << "Hello User, " << endl;
+    cout << "Thank you for joining me today! " << endl; 
+    cout << "1) login: " << endl; 
+    cout << "2) Sign up: " << endl;
+    cin >> input; 
     
+    switch(input)
+    {
+        case 1: 
+            //login();
+            break;
+        case 2:
+            signUp(tempUsers);
+            break;
+        default:
+            cout << "Wrong value";
+    }
+}
+
+/*void login()
+{
+    char userName;
+    char userPassword;
+    cout << "User Name: " << endl;
+    cin >> userName; 
+    cout << "Password: " << endl;
+    cin >> userPassword;
+    fstream f("userinfo.bin", ios::out | ios::binary);
+
+    if(f)
+    {
+        f.read(userName, userPassword);
+
+        f.close();
+    }
+    //Gets User Input and compares it to the data stored in the binary file 
+    //If the data is not found in binary file then the User does not exist
+    /*
+     *Need to get this function to read from a binary file 
+     *I don't understand the read function and how it works Need to look further 
+     *into the function
+     */ 
+//}
+//*/
+
+void signUp(Users tempUser)
+{
+    //the userName and password should be taken from the class  
+    string userName;
+    string userPassword;  
+    cout << "Hello New User" << endl;
+    cout << "We will need a user name and password" << endl; 
+    cout << "User Name: " << endl;
+    cin >> userName; 
+    cout << "Password: " << endl; 
+    cin >> userPassword; 
+
+    ofstream f; 
+    f.open("userinfo.dat", ios::binary | ios::out);
+
+    //Convert the tempUser.userName from a string to a char* 
+    strcpy(tempUser.n, userName);
+    strcpy(tempUser.p, userPassword);
+    
+
+
+    if(f)
+    {
+        f.write(reinterpret_cast<char*>(&n), sizeof(tempUser)); //I do not understand how this works?
+        f.write(reinterpret_cast<char*>(&p), sizeof(tempUser));
+
+        f.close();
+    }
+    else
+    {
+        cout << "Error\n";
+    }
+    
+    //Gets User Input and adds it to the binary file 
+    //Checks to see if user is already added to bin file 
+    //If not then add the new User info  
 }
 
 // Function purpose is to get the item choosen by the user 
